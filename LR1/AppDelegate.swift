bfe9076e1da0c0492f7577ee0329451f0e816d83
Станислав Зверьков <zverk3333@gmail.com>
2022-03-14 02:27:52 +0300

    Initial Commit

---
+++ LR1/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LR1
+//
+//  Created by Станислав Зверьков on 14.03.2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ LR1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LR1
+//
+//  Created by Станислав Зверьков on 14.03.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
